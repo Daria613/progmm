e4f8a973e237363fba722324870787bfa27b45f8
Daria613 <batrakovva9@gmail.com>
2024-10-16 11:30:30 +0300

    Create Lab2Work2.js

---
+++ JavaScript/Lab2Work2.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let z 
+let y = +prompt(("y = "));  
+let a = +prompt(("a = ")); 
+let e = +prompt(("e = ")); 
+let x = +prompt(("x = ")); 
+if (x * y > 0){ 
+    z = 1 - Math.pow(e, x*y + a*b)  
+} 
+if ( (x * y) = 0 ){ 
+    z = b - Math.min(a * x) 
+}    
+z = Math.min 
+if (x * y< 0){ 
+    z= Math.max(Math.pow(x, 3)) 
+} 
+console.log(z);
