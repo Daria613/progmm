be16c1e31316a12dba31c51b6f15f5a6281edff7
Daria613 <batrakovva9@gmail.com>
2024-10-17 11:39:55 +0300

    Create laboratornai3v3.cpp

---
+++ C++/laboratornai3v3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double y;
+	cout << "Использование цикла for" << endl;
+	cout << "--------------------" << endl << endl;
+	// С использованием цикла for
+	for (double p = -1; p <= 1; p += 0.1)
+	{
+		y = 0.1 * pow(2, -p) * exp(p);
+		cout << y << endl;
+	}
+	cout << "Использование цикла While" << endl;
+	cout << "--------------------" << endl << endl;
+	// С использованием цикла While
+	double x = -1;
+	while (x <= 1)
+	{
+		y = 0.1 * pow(2, -x) * exp(x);
+		x += 0.1;
+		cout << y << endl;
+	}
+	cout << "Использование цикла do While" << endl;
+	cout << "--------------------" << endl << endl;
+	// С использованием do While
+	x = 0;
+	do
+	{ 
+		y = 0.1 * pow(2, -x) * exp(x);
+		x += 0.1;
+		cout << y << endl;
+	} while (x <= 1);
+	
+}
