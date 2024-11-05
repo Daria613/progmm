ee4054db397b7b664396c9c545e0022d99b2a4ec
Daria613 <batrakovva9@gmail.com>
2024-11-05 22:51:29 +0300

    Create massive.cs

---
+++ C# /massiv.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // тип_данных [,] имя_массива;
+            int[,] dva = new int[3, 5]
+                {
+                   {2,45,12,51,51},
+                   {23,87,3,6,10},
+                   {41,54,50,9,7}
+
+
+                };
+            
+            Console.WriteLine(dva[0,2]);
+        } 
+    }
+}
