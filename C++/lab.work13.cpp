8081b84289a8567f74ff21ec036e53e1b8b99269
Daria613 <batrakovva9@gmail.com>
2024-11-14 18:04:14 +0300

    Update lab.work13.cpp

--- C++/lab.work13.cpp
+++ C++/lab.work13.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
-double Opereic(double, double,double);
+double Opereic(double, double, double);
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	double y;
+
 	double a;
 	cin >> a;
+
 	double a2;
 	cin >> a2;
 
@@ -15,17 +17,24 @@ int main()
 
 	double c2;
 	cin >> c2;
+
 	y = Opereic(a2, c2, b2);
 	cout << y << endl;
 }
+
+
 double Opereic(double a2, double c2, double b2)
 {
 	cout << endl << 180 << endl;
-	
+
 	if (a2 < 59 || a2 > 61)
 	{
-		cout << "Ошибка повторите ввод" << endl; 
+		cout << "Ошибка повторите ввод" << endl;
 	}
+
+
+
+
 	cout << endl << 180 << endl;
 
 	if (b2 < 59 || b2 > 61)
@@ -34,14 +43,21 @@ double Opereic(double a2, double c2, double b2)
 		cout << "Ошибка повторите ввод" << endl;
 	}
 
+
+
+
+	cout << endl << 180 << endl;
 	if (c2 < 59 || c2 > 61)
 	{
 		cout << "Ошибка повторите ввод" << endl;
 	}
-	double Opereic(double a,  double c2,double b2);
+	double Opereic(double a, double c2, double b2);
 
-	 {
+
+
+
+	{
 		double a = c2 / sin(b2);
 		return a;
-	 }
-} 
+	}
+}
