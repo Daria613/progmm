a5bfdc9a4542427d44091b142d56602cdb5f5ddd
Daria613 <batrakovva9@gmail.com>
2025-09-30 10:25:41 +0300

    massivwospominanie.cs

---
+++ C# /massivwospominanie.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 2, 3, 1, 0, 10, 28, 27, 31 };
+            List<int> b = new List<int>(10);
+            int h = a[0];
+            b.Add(h); 
+         
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (h < a[i])
+                {
+                    b.Add(h);
+                }
+               
+            }
+
+            for (int i = 0; i< b.Count; i++)
+            {
+                Console.WriteLine(b[i]);
+            }
+           
+        }
+    }
+}
